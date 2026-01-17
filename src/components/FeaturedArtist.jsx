c39b8f89514fc7fccdc1c2cb651eafab5a4db3b5
alanwatts07 <morpheus@alanbox.localdomain>
2026-01-17 13:38:56 -0500

    Update Spotify Embed to Album

--- src/components/FeaturedArtist.jsx
+++ src/components/FeaturedArtist.jsx
@@ -30,7 +30,7 @@ const FeaturedArtist = () => {
                         <iframe
                             data-testid="embed-iframe"
                             style={{ borderRadius: '12px' }}
-                            src="https://open.spotify.com/embed/artist/4nFnZEANBT2uxw3aI2DK1Z?utm_source=generator&theme=0"
+                            src="https://open.spotify.com/embed/album/4dbj1aQzTCJHHaXFBbZpJe?utm_source=generator&theme=0"
                             width="100%"
                             height="352"
                             frameBorder="0"
